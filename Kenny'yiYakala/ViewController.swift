3c7a2ec57b25d0113b71d540a3a7c8ef8de0c331
doguner1 <d.gunr@yaani.com>
2023-02-05 22:55:59 +0300

    Initial Commit

---
+++ Kenny'yiYakala/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kenny'yiYakala
+//
+//  Created by 'toMAC on 5.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
